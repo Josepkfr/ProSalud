65c8baa725ed6f345da6002a6a2582bc7fc1ce28
Jose Alpala <alpalajose@gmail.com>
2021-08-03 13:18:54 -0500

    Landing page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ const app = express();
 
 //settings
 app.set('port',process.env.PORT || 3000);
+//motor de plantillas
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 //midlewares
 app.use(morgan('dev'))
 
+app.get('/',(req,res)=>{
+    res.render("index",{titulo:'titulo dinamico'})
+})
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(app.get('port'),()=>{
 console.log('server on port 3000')
